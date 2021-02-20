75719be812713d2e66ce7dc85494192511069498
doduck210 <doduck2@gmail.com>
2021-02-20 22:29:42 +0900

    programmers

---
+++ 42578.cpp
@@ -0,0 +1,36 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42578?language=cpp
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    vector<string> types;
+    vector<int> ans;
+    
+    for(int i=0;i<clothes.size();i++){
+        
+        bool no=true;
+        for(int j=0;j<types.size();j++){
+            if(clothes[i][1]==types[j]){
+                no=false;
+                ans[j]++;
+                break;
+            }
+        }
+        if(no){
+            types.push_back(clothes[i][1]);
+            ans.push_back(1);
+        }
+        
+    }
+    
+    
+    
+    int answer = 1;
+    for(int i=0;i<ans.size();i++){
+        answer=answer*(ans[i]+1);
+    }
+    
+    return answer-1;
+}
