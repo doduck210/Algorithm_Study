e065733265a9e0a88fdf675921a6853478fc723f
doduck210 <doduck2@gmail.com>
2021-05-16 23:52:25 +0900

    programmers

---
+++ 42586.cpp
@@ -0,0 +1,22 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42586?language=cpp
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> ans;
+    int cur=-1;
+    for(int i=0;i<progresses.size();i++){
+        int needed=(100-progresses[i])/speeds[i];
+        if((100-progresses[i])%speeds[i]!=0) needed++;
+        if(cur<needed){
+            ans.push_back(1);
+            cur=needed;
+        }
+        else{
+            ans[ans.size()-1]++;
+        }
+    }
+    return ans;
+}
