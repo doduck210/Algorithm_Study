669523050705cd33979e6b6d9b467822584c889b
doduck210 <doduck2@gmail.com>
2023-01-22 19:11:37 +0900

    programmers

---
+++ 12947.cpp
@@ -0,0 +1,15 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12947
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    int sum=0;
+    int xx=x;
+    while(xx){
+        sum+=xx%10;
+        xx/=10;
+    }
+    return x%sum==0;
+}
