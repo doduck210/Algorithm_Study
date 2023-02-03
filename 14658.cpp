9a85a8663b470932b8a31bfcf3098bd8d13ad39b
doduck210 <doduck2@gmail.com>
2023-02-03 09:49:36 +0900

    BOJ not solved yet

--- 14658.cpp
+++ 14658.cpp
@@ -4,15 +4,13 @@
 using namespace std;
 
 int N,M,L,K;
-bool table[500001][500001]={0,};
-int mxTable[500001][500001]={0,};
+vector<int> k;
 
 void input(){
     cin>>N>>M>>L>>K;
     for(int i=0;i<K;i++){
-        int y,x;
-        cin>>y>>x;
-        table[y][x]=1;
+        int kk; cin>>kk;
+        k.push_back(kk);
     }
 }
 
