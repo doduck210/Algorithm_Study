e8b27bc765d4c8b56c1eb2cbb9d496ef972a64a8
doduck210 <doduck2@gmail.com>
2023-02-19 11:54:39 +0900

    BOJ

--- 2096.cpp
+++ 2096.cpp
@@ -2,46 +2,31 @@
 #include <algorithm>
 using namespace std;
 int N;
-int table[100000][3];
-int minTable[100000][3];
-int maxTable[100000][3];
+int a=0,b=0,c=0;
+int aa=0,bb=0,cc=0;
 
 void input(){
     cin>>N;
     for(int i=0;i<N;i++){
-        cin>>table[i][0]>>table[i][1]>>table[i][2];
-    }
-    //initialize first line
-    minTable[0][0]=table[0][0]; minTable[0][1]=table[0][1]; minTable[0][2]=table[0][2];
-    maxTable[0][0]=table[0][0]; maxTable[0][1]=table[0][1]; maxTable[0][2]=table[0][2];
-}
-
-void solution(){
-    for(int i=1;i<N;i++){
-        for(int j=0;j<3;j++){
-            if(j==0){
-                maxTable[i][j]=max(maxTable[i-1][0],maxTable[i-1][1])+table[i][0];
-                minTable[i][j]=min(minTable[i-1][0],minTable[i-1][1])+table[i][0];
-            }
-            else if(j==1){
-                maxTable[i][j]=max(maxTable[i-1][0],maxTable[i-1][1]);
-                maxTable[i][j]=max(maxTable[i][j],maxTable[i-1][2])+table[i][1];
-                minTable[i][j]=min(minTable[i-1][0],minTable[i-1][1]);
-                minTable[i][j]=min(minTable[i][j],minTable[i-1][2])+table[i][1];
-            }
-            else{
-                maxTable[i][j]=max(maxTable[i-1][2],maxTable[i-1][1])+table[i][2];
-                minTable[i][j]=min(minTable[i-1][2],minTable[i-1][1])+table[i][2];
-            }
-        }
+        int ta,tb,tc;
+        cin>>ta>>tb>>tc;
+        //cal min
+        int na=min(a,b)+ta;
+        int nb=min(a,b); nb=min(nb,c)+tb;
+        int nc=min(b,c)+tc;
+        a=na;b=nb;c=nc;
+        //cal max
+        na=max(aa,bb)+ta;
+        nb=max(aa,bb); nb=max(nb,cc)+tb;
+        nc=max(bb,cc)+tc;
+        aa=na;bb=nb;cc=nc;
     }
 }
 
 int main(){
     input();
-    solution();
-    int mx=max(maxTable[N-1][0],maxTable[N-1][1]); mx=max(mx,maxTable[N-1][2]);
-    int mn=min(minTable[N-1][0],minTable[N-1][1]); mn=min(mn,minTable[N-1][2]);
+    int mx=max(aa,bb); mx=max(mx,cc);
+    int mn=min(a,b); mn=min(mn,c);
     cout<<mx<<" "<<mn<<"\n";
     return 0;
 }
