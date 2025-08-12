c790e642cda5bd70d3411c6e51398c7964122ad1
ducky <duck@sbs.co.kr>
2025-08-12 05:12:10 +0000

    BOJ - out of memory solved

--- 1967.cpp
+++ 1967.cpp
@@ -1,7 +1,9 @@
 //https://www.acmicpc.net/problem/1967
+//out of memory at the first try. bfs twice is a solution.
 #include <iostream>
 #include <utility>
 #include <vector>
+#include <queue>
 #include <algorithm>
 using namespace std;
 
@@ -9,44 +11,58 @@ int n;
 
 vector<pair<int,int>> table[10001];
 
+int farNode, farLength;
+
+void bfs(int startNode){
+    //bfs로 startNode에서 가장 떨어진 곳의 노드번호, 길이 반환
+    queue<pair<int,int>> q;
+    farLength=0;
+    bool visited[10001]={false,};
+    visited[startNode]=true;
+
+    for(int i=0;i<table[startNode].size();i++){
+        q.push(table[startNode][i]);
+    }
+
+    while(!q.empty()){
+        int curNode=q.front().first;
+        int curWeight=q.front().second;
+        q.pop();
+        visited[curNode]=true;
+        //Leaf 도착시엔 length처리
+        if(table[curNode].size()==1){
+            if(farLength<curWeight){
+                farLength=curWeight;
+                farNode=curNode;
+            }
+            continue;
+        }
+        //다른 연결된 곳 queue에 집어넣기
+        for(int i=0;i<table[curNode].size();i++){
+            if (visited[table[curNode][i].first]) continue;
+            else{
+                q.push({table[curNode][i].first,table[curNode][i].second+curWeight});
+            }
+        }
+    }
+}
+
 int main(){
     cin>>n;
-    //입력받고 테이블 채우기
+    //입력받고 가중치 저장
     for(int i=0;i<n-1;i++){
         int parent, child, weight;
         cin>>parent>>child>>weight;
         
-        //가중치 넣어주고
         table[child].push_back({parent,weight});
-        
-        //부모노드에 있는 가중치로 자식 거리 업데이트
-        for(int j=0;j<table[parent].size();j++){
-            int otherchild=table[parent][j].first;
-            int otherweight=table[parent][j].second;
-            
-            table[child].push_back({otherchild,otherweight+weight});
-            table[otherchild].push_back({child,otherweight+weight});
-        }
-
         table[parent].push_back({child,weight});
+
     }
-    
-    /*디버깅용 각 노드별 거리 출력
-    for(int i=1;i<=n;i++){
-        cout<<"node "<<i<<endl;
-        for(int j=0;j<table[i].size();j++){
-            cout<<'<'<<table[i][j].first<<' '<<table[i][j].second<<">\n";
-        }
-        cout<<"\n";
-    }
-    */
-   int ans=0;
-    for(int i=1;i<=n;i++){
-        for(int j=0;j<table[i].size();j++){
-            ans=max(ans,table[i][j].second);
-        }
-    }
-    cout<<ans<<'\n';
+
+    //bfs 두번
+    bfs(1);
+    bfs(farNode);
+    cout<<farLength<<endl;
 
     return 0;
 }
